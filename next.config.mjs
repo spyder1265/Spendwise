43447d55fbf9105c8f72ea52dd90329abdb614e0
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2023-12-31 16:58:30 +0000

    ei

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,12 @@ const config = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;
